68961c7fe7d8869e9fc62d92981924b9bee19231
JLB-P <joseluis.barbosap@gmail.com>
2023-05-04 14:33:07 -0600

    upd040523

---
+++ sdk_2023_2/sdk_2023_2/a_comp.h
@@ -0,0 +1,14 @@
+﻿/*
+ * A_COMP.h
+ *
+ * Created: 15/03/2016 06:56:09 a. m.
+ *  Author: JLB
+ */ 
+
+
+#ifndef a_comp_H_
+#define a_comp_H_
+
+void init_analog_comp(void);
+
+#endif /* a_comp_H_ */
---
+++ sdk_2023_2/sdk_2023_2/a_comp.h.bak
@@ -0,0 +1,14 @@
+﻿/*
+ * A_COMP.h
+ *
+ * Created: 15/03/2016 06:56:09 a. m.
+ *  Author: JLB
+ */ 
+
+
+#ifndef A_COMP_H_
+#define A_COMP_H_
+
+void Init_ANA_COMP(void);
+
+#endif /* A_COMP_H_ */
